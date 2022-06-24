b5a5d9a3e394924c6202cf1ad5d689a7af300a79
someniatko <someniatko@gmail.com>
2022-06-24 20:44:44 +0300

    fix tests to pass Psalm validation

--- test/ResultTest.php
+++ test/ResultTest.php
@@ -6,14 +6,17 @@ namespace Test;
 
 use PHPUnit\Framework\TestCase;
 use Someniatko\ResultType\Error;
+use Someniatko\ResultType\ResultInterface;
 use Someniatko\ResultType\Success;
 
 final class ResultTest extends TestCase
 {
     public function testReadmeExample(): void
     {
-        $value = (new Success('Let it be'))
-            ->map(fn (string $s) => substr_count($s, ' '))
+        /** @var ResultInterface<string, string> $initial */
+        $initial = new Success('Let it be');
+        $value = $initial
+            ->map(fn (string $s): int => substr_count($s, ' '))
             ->chain(
                 fn (int $wordsCount) => $wordsCount > 3
                     ? new Success('Long text')
@@ -28,42 +31,48 @@ final class ResultTest extends TestCase
 
     public function testMapWithSuccess(): void
     {
+        /** @var ResultInterface<int, int> $initial */
         $initial = new Success(1500);
         $mapped = $initial->map(fn (int $val) => $val / 2);
-        self::assertSame(750, $mapped->get());
+        self::assertEquals(750, $mapped->get());
     }
 
     public function testMapWithError(): void
     {
+        /** @var ResultInterface<int, int> $initial */
         $initial = new Error(1500);
         $mapped = $initial->map(fn (int $val) => $val / 2);
-        self::assertSame(1500, $mapped->get());
+        self::assertEquals(1500, $mapped->get());
     }
 
     public function testMapErrorWithSuccess(): void
     {
+        /** @var ResultInterface<int, int> $initial */
         $initial = new Success(1500);
         $mapped = $initial->mapError(fn (int $val) => $val / 2);
-        self::assertSame(1500, $mapped->get());
+        self::assertEquals(1500, $mapped->get());
     }
 
     public function testMapErrorWithError(): void
     {
+        /** @var ResultInterface<int, int> $initial */
         $initial = new Error(1500);
         $mapped = $initial->mapError(fn (int $val) => $val / 2);
-        self::assertSame(750, $mapped->get());
+        self::assertEquals(750, $mapped->get());
     }
 
     public function testMapAfterChainReturningSuccessUsingSuccess(): void
     {
+        /** @var ResultInterface<string, string> $initial */
         $initial = new Success('Hello');
         $chained = $initial->chain(fn (string $successResult) => new Success($successResult . ', world!'));
         $mapped = $chained->map('strtoupper');
-        self::assertSame('HELLO, WORLD!', $mapped->get());
+        self::assertEquals('HELLO, WORLD!', $mapped->get());
     }
 
     public function testMapAfterChainReturningErrorUsingSuccess(): void
     {
+        /** @var ResultInterface<string, string> $initial */
         $initial = new Success('Hello');
         $chained = $initial->chain(fn (string $successResult) => new Error($successResult . ', error occurred!'));
         $mapped = $chained->map('strtoupper');
@@ -72,9 +81,10 @@ final class ResultTest extends TestCase
 
     public function testMapAfterChainReturningSuccessUsingError(): void
     {
+        /** @var ResultInterface<string, string> $initial */
         $initial = new Error('Hello');
         $chained = $initial->chain(fn (string $successResult) => new Success($successResult . ', world!'));
         $mapped = $chained->map('strtoupper');
-        self::assertSame('Hello', $mapped->get());
+        self::assertEquals('Hello', $mapped->get());
     }
 }
