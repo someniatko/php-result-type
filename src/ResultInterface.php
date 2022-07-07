ca38cd7f94d229b7bd5fb9ab1c19e191243dff68
someniatko <someniatko@gmail.com>
2022-07-08 00:00:13 +0300

    allow non-pure callables again

--- src/ResultInterface.php
+++ src/ResultInterface.php
@@ -20,7 +20,7 @@ interface ResultInterface
      *
      * @template TNewSuccess
      *
-     * @param pure-callable(TSuccess):TNewSuccess $map
+     * @param callable(TSuccess):TNewSuccess $map
      * @return self<TNewSuccess, TError>
      */
     public function map(callable $map): self;
@@ -34,7 +34,7 @@ interface ResultInterface
 
      * @template TNewError
      *
-     * @param pure-callable(TError):TNewError $map
+     * @param callable(TError):TNewError $map
      * @return self<TSuccess, TNewError>
      */
     public function mapError(callable $map): self;
@@ -47,7 +47,7 @@ interface ResultInterface
      * @template TNewSuccess
      * @template TNewError
      *
-     * @param pure-callable(TSuccess):self<TNewSuccess, TNewError> $map
+     * @param callable(TSuccess):self<TNewSuccess, TNewError> $map
      * @return self<TNewSuccess, TError|TNewError>
      */
     public function chain(callable $map): self;
@@ -68,7 +68,7 @@ interface ResultInterface
      *
      * @template TNewError
      *
-     * @param pure-callable(TError):TNewError $map
+     * @param callable(TError):TNewError $map
      * @return TSuccess|TNewError
      */
     public function getOr(callable $map);
